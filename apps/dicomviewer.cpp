9806eb7def2c0c399d9b80688ae1db1a6b9304b7
beyondXin <beyondXin>
2021-01-22 22:16:42 +0800

    1. 样式表修改 2. 增加dcm可标签视化（仅增加测试代码)

--- apps/dicomviewer.cpp
+++ apps/dicomviewer.cpp
@@ -1,9 +1,9 @@
-﻿//#include <global/KissGlobal>
-//#include <engine/KissEngine>
-//#include <window/KissWindow>
+﻿#include <global/KissGlobal>
+#include <engine/KissEngine>
+#include <window/KissWindow>
 
 ////----------------------------------------------------------------
-//int main(int argc, char *argv[]) {
+//int main1(int argc, char *argv[]) {
 //    QApplication a(argc, argv);
 //    a.setOrganizationName(Project_OrganizationName);
 //    a.setOrganizationDomain(Project_OrganizationDomain);
@@ -11,23 +11,21 @@
 //    a.setQuitOnLastWindowClosed(true);
 //    using namespace Kiss;
 //    QUIHelper::SetStyle(":/Style/style.qss");
-//    QUIHelper::SetCode();
-//    InitialDir();
-//    InitialDao();
-//    DicomViewer w(DicomViewer::SingleInstance);
-//    w.show();
+////    QUIHelper::SetCode();
+////    InitialDir();
+////    InitialDao();
+////    DicomViewer w(DicomViewer::SingleInstance);
+////    w.show();
+//    QTreeWidgetItem item;
+//    item.setText(0, "aa");
+//    QTreeWidgetItem item1;
+//    item1.setText(0, "aa");
+//    item.addChild(&item1);
+//    //
 //    return a.exec();
 //}
 
-
 #include "dcmtk/dcmdata/dctk.h"
-
-#define Print std::cout<<
-#define Printend <<std::endl;
-#define PrintemspValue(emsp,value1,value2,value3,value4,value5,value6) \
-    for(int i = 0; i < emsp; i++) {Print "\40\40";}\
-    Print value1 <<" "  <<value2<<" ["<< value3 <<"] "\
-                 << value4 <<" " <<value5 <<" (" <<value6<<")" Printend
 
 template<typename T>
 class NewDcmItem: public T {
@@ -44,8 +42,9 @@ using MyDcmDataset = NewDcmItem<DcmDataset>;
 using MyDcmMetaInfo = NewDcmItem<DcmMetaInfo>;
 using MywDcmItem = NewDcmItem<DcmItem>;
 
+//
 template<typename T>
-void PaintTags(T &t, const int &emsp = 0) {
+void GenerateItems(QList<QTreeWidgetItem *> &items, T &t) {
     DcmList *elementList = t.GetDcmList();
     if (!elementList->empty()) {
         DcmObject *dO;
@@ -58,26 +57,64 @@ void PaintTags(T &t, const int &emsp = 0) {
             DcmElement *elem;
             t.findAndGetElement(tag, elem);
             elem->getOFString(value, 0);
-            PrintemspValue(emsp, tag, tag.getVRName(),
-                           value, dO->getLength(), dO->getVM(), tag.getTagName())
+            QTreeWidgetItem *tmp_item = new QTreeWidgetItem;
+            tmp_item->setText(0, QString::fromLocal8Bit(tag.toString().c_str()));
+            tmp_item->setText(1, tag.getVRName());
+            tmp_item->setText(2, QString::number(dO->getLength()));
+            tmp_item->setText(3, QString::number(dO->getVM()));
+            tmp_item->setText(4, tag.getTagName());
+            tmp_item->setText(5, QString::fromLocal8Bit(value.c_str()));
             if(EVR_SQ == dO->getVR()) {
+                QList<QTreeWidgetItem *> tmp_items;
                 DcmItem *sq;
                 t.findAndGetSequenceItem(dO->getTag(), sq);
                 MywDcmItem *dcmitem_info = new MywDcmItem(*sq);
-                PaintTags(*dcmitem_info, emsp + 1);
+                GenerateItems(tmp_items, *dcmitem_info);
+                tmp_item->addChildren(tmp_items);
             }
+            items << tmp_item;
         } while (elementList->seek(ELP_next));
         delete dO;
     }
 }
 
-int main() {
+QTreeWidget *GetWid() {
+    QTreeWidget *wid = new QTreeWidget;
+    wid->setHeaderLabels(QStringList()
+                         << "Tag ID" << "VR" << "VM" << "Length" << "Description" << "value");
+    wid->setColumnWidth(0, 200);
+    wid->setColumnWidth(1, 70);
+    wid->setColumnWidth(2, 100);
+    wid->setColumnWidth(3, 50);
+    wid->setColumnWidth(4, 300);
+    wid->setColumnWidth(5, 300);
+    wid->setGeometry(0, 0, 1200, 800);
+    return wid;
+}
+
+int main(int argc, char *argv[]) {
+    QApplication a(argc, argv);
+    using namespace Kiss;
+    QUIHelper::SetStyle(":/Style/style.qss");
+    // 初始化 TreeWidget
+    QTreeWidget *wid = GetWid();
+    QList<QTreeWidgetItem *> items;
+    // 读取文件
     DcmFileFormat dfile;
-    dfile.loadFile("/home/arteryflow/图片/DicomData/DSA/Liyunlong/IMG-0002-00001.dcm");
-    Print "DcmMetaInfo------" Printend
+    dfile.loadFile("C:/Users/77935/Pictures/dcm/source.dcm");
+    //
+    QTreeWidgetItem *metainfo_item = new QTreeWidgetItem;
+    metainfo_item->setText(0, "Dicom-MetaInfo");
+    items << metainfo_item;
     MyDcmMetaInfo *meta_info = new MyDcmMetaInfo(*dfile.getMetaInfo());
-    PaintTags(*meta_info);
-    Print "DcmDataset------" Printend
+    GenerateItems(items, *meta_info);
+    QTreeWidgetItem *dataset_item = new QTreeWidgetItem;
+    dataset_item->setText(0, "Dicom-Data-Set");
+    items << dataset_item;
     MyDcmDataset *dataset_info = new MyDcmDataset(*dfile.getDataset());
-    PaintTags(*dataset_info);
+    GenerateItems(items, *dataset_info);
+    // 显示
+    wid->addTopLevelItems(items);
+    wid->show();
+    return a.exec();
 }
